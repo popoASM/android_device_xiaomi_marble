98e4f1d98f2a27ee8018e2d84388776ea52fc776
popoA3M <pratyayaborborah@gmail.com>
2023-11-11 21:46:19 +0530

    marble: PixelOS bringup
    - also revert f0f8039
    
    Signed-off-by: popoA3M <pratyayaborborah@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,13 +26,13 @@ AB_OTA_PARTITIONS += \
 
 # Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv9-a
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := cortex-a510
+TARGET_CPU_VARIANT := cortex-a76
 
 TARGET_2ND_ARCH := arm
-TARGET_2ND_ARCH_VARIANT := armv8-2a
+TARGET_2ND_ARCH_VARIANT := armv8-a
 TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a76
@@ -109,9 +109,7 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := \
     video=vfb:640x400,bpp=32,memsize=3072000 \
     disable_dma32=on \
-    kpti=off \
-    swinfo.fingerprint=$(AWAKEN_VERSION) \
-    mtdoops.fingerprint=$(AWAKEN_VERSION)
+    kpti=off
 
 BOARD_BOOTCONFIG := \
     androidboot.hardware=qcom \
@@ -241,7 +239,7 @@ DEVICE_MANIFEST_UKEE_FILES := \
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
     $(DEVICE_PATH)/configs/vintf/vendor_framework_compatibility_matrix.xml \
     $(DEVICE_PATH)/configs/vintf/xiaomi_framework_compatibility_matrix.xml \
-    vendor/awaken/config/device_framework_matrix.xml
+    vendor/aosp/config/device_framework_matrix.xml
 ODM_MANIFEST_SKUS += marble
 ODM_MANIFEST_MARBLE_FILES := $(DEVICE_PATH)/configs/vintf/manifest_nfc.xml
 
--- aosp_marble.mk
+++ aosp_marble.mk
@@ -8,20 +8,20 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Awaken stuff.
-$(call inherit-product, vendor/awaken/config/common_full_phone.mk)
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Inherit from marble device.
 $(call inherit-product, device/xiaomi/marble/device.mk)
+
+# Boot animation
+TARGET_BOOT_ANIMATION_RES := 1080
 
 ## Device identifier
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := marble
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := awaken_marble
+PRODUCT_NAME := aosp_marble
 
 # GMS
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Derp
-TARGET_SUPPORTS_QUICK_TAP := true
